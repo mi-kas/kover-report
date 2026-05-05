c3da81d23967d9a1007826d97034092c55f4a1c8
Michael Kaserer <mi-kas@users.noreply.github.com>
2026-05-05 13:47:56 +0200

    Fix packaging issue

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,7 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 import {run} from './action'
 
-const importModule = new Function('specifier', 'return import(specifier)') as <
-  T
->(
-  specifier: string
-) => Promise<T>
-
-async function main() {
-  const core =
-    await importModule<typeof import('@actions/core')>('@actions/core')
-  const github =
-    await importModule<typeof import('@actions/github')>('@actions/github')
-
-  try {
-    await run(core, github)
-  } catch (error: unknown) {
-    core.setFailed((error as Error).message)
-  }
-}
-
-void main()
+void run(core, github).catch((error: unknown) => {
+  core.setFailed((error as Error).message)
+})
